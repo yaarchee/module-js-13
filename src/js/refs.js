2226a392e52ab49cd86180e8671e5afe6d75a75f
Yaroslav Paltsun <trancemanjake@gmail.com>
2020-09-16 01:03:54 +0300

    Updates

--- src/js/refs.js
+++
@@ -1,9 +0,0 @@
-
-const  refs ={
-  imagesList: document.querySelector('.images-list'),
-  form: document.querySelector('.form'),
-  imageListItem: document.querySelectorAll('.images-list__item'),
-
-}
-
-export default refs;
